31bbf32b27000ecb1b269301fefe6780fe673b0b
jukensoudan-route <route.jukensoudan@gmail.com>
2026-07-20 18:12:58 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='iburi-family-v16-13';
+const CACHE_VERSION='iburi-family-v17-13';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil((async()=>{
